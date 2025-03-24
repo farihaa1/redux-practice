3d5c709df3a43559022ceae2809ce29a3b5635e0
farihaa1 <farihaa1403@gmail.com>
2025-03-24 08:49:22 +0600

    started a basic crud app with redux toolkit

---
+++ book-crud-with-redux-toolkit/src/Components/BookList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BookList = () => {
+    return (
+        <div>
+           <h2>List of Books</h2> 
+        </div>
+    );
+};
+
+export default BookList;
---
+++ book-crud-with-redux-toolkit/src/features/bookSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  books: [],
+};
+
+const bookSlice = createSlice({
+  name: "books",
+  initialState,
+  reducers: {
+
+  },
+});
+
+export default bookSlice.reducer
