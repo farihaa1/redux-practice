d2ebeddf428087398e4453710a40ba082ee42511
farihaa1 <farihaa1403@gmail.com>
2025-03-26 12:08:49 +0600

    added post view component

--- async-redux-practice/src/features/PostView.jsx
+++ async-redux-practice/src/features/PostView.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchPosts } from "./Posts/postSlice";
 
 const PostView = () => {
-    return (
-        <div>
-            <h2>Posts</h2>
-            
-        </div>
-    );
+  const dispatch = useDispatch();
+
+  const { isLoading, error, posts } = useSelector((state) => state.posts);
+
+  useEffect(() => {
+    dispatch(fetchPosts());
+  });
+
+  return (
+    <div>
+      <h2>Posts</h2>
+      {isLoading && <h3>Loading</h3>}
+      {error && <h3>{error}</h3>}
+      <section>
+        {posts &&
+          posts.map((post) => (
+            <article key={post.id}>
+              <h5>{post.title}</h5>
+              <p>{post.body}</p>
+            </article>
+          ))}
+      </section>
+    </div>
+  );
 };
 
 export default PostView;
